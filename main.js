812144b1234a92295547f994bb6a95a2efddb4af
otsembo <okumu.otsembo@gmail.com>
2024-01-23 20:55:55 +0300

    finish fetch class

--- main.js
+++ main.js
@@ -48,4 +48,52 @@ window.addEventListener('DOMContentLoaded', () => {
         return gridItem;
     }
 
+    // rejection UI
+    const createErrorUi = (reason) => {
+        const rootDiv = document.createElement("h1");
+        rootDiv.innerText = reason
+        return rootDiv;
+    }
+
+    // make network request
+    const request = fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=991a1731e00a4b5a83bb6b22ca744fcc")
+    request.then(
+        (result) => {
+            console.log("result", result)
+            return result.json()
+        }
+    ).then(
+        (body) => {
+            const articles = body["articles"]
+
+            for(let i = 0; i < articles.length; i++){
+
+                const currentArticle = articles[i];
+                
+                const title = currentArticle["title"];
+                const desc = currentArticle["description"];
+                const image = currentArticle["urlToImage"];
+                const link = currentArticle["url"];
+
+                const articleCard = createNewsCard(
+                    title, desc, image, link
+                )
+
+                grid.appendChild(articleCard);
+
+
+            }
+
+        }
+    ).catch(
+        (reason) => {
+            const errorMessage = createErrorUi(reason);
+            grid.appendChild(errorMessage);
+        }
+    ).finally(
+        () => {
+            console.log("promise has completed whether rejected or not")
+        }
+    )
+
 });
